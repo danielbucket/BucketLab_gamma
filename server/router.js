103c87391e3991ffa452aa1fde4b73e149680ffb
Daniel Bucket <danieljbucket@gmail.com>
2024-11-19 21:43:18 -0700

    simple server

--- server/router.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { GET_homeContent } = require('./controllers/Home_GET_controller');
-
-router.get('/home', GET_homeContent);
-
-module.exports = router;
