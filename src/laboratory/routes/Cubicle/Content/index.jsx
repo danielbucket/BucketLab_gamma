d18f8dca01dcb7db0db53ec065ac4ea760c863fe
Daniel Bucket <danieljbucket@gmail.com>
2025-03-03 08:02:25 -0700

    Trying to resolve a build failure (only when depolying to Cloudflare). Seemingly the Cubicle component could not resolve the Sidebar component when trying to load the file. I cant imagine why, everything is correct. So I removed the filed import and its use in the return statement from Cubicle. I dont know what else to do. I also updated the landing page upon user login

--- src/laboratory/routes/Cubicle/Content/index.jsx
+++ src/laboratory/routes/Cubicle/Content/index.jsx
@@ -10,11 +10,16 @@ export default function Content() {
         <p>Coming soon to a Cubicle near you...</p>
         <p>In the works is a communication utility that will bridge the gap between you the Cubicle Dweller and me, Bucket.</p>
         <p>Stay tuned for updates!</p>
-        <p>I need to clear out the cobwebs currently inhabiting the space in my brain where my knowledge of KnexJS and PosgreSQL should still be burried...</p>
+      </div>
+      <div className="development-update">
+        <p>I'm currently learning my way around MongoDB. I thought I was going to relearn KnexJS, but it seems that Mongo / MongooseJS was to be the better choice.</p>
+        <p>So, I'm learning MongoDB and MongooseJS. But the rabbit hole deepens; I'm also learning how to use, build, and deploy Ubuntu Server (on a raspberry PI5), Docker, and Cloudflare. </p>
+        <p>The BucketLab api, server, and database are both isolated in a single Docker Container being served from Ubuntu Server on a Raspberry Pi5. In my home.</p>
+        <p>This container is connected to the world wide web via Cloudflare (where BucketLab.io is hosted) using Cloudflared Tunnels.</p>
+        <p>It's so much fun.</p>
       </div>
       <div className='footer'>
         <p>Remember, I can still be contacted via <Link to='https://www.linkedin.com/in/daniel-ludwick/'>LinkedIn</Link></p>
-        <p>*No data has been or is being collected at this time.</p>
       </div>
     </StyledContent>
   )
